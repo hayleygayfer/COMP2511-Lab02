71ceb4e194e00ac39e4e8a61345fda033ed3708c
Hayley Gayfer <z5312096@ad.unsw.edu.au>
2021-06-13 12:58:19 +1000

    wrote more tests

--- src/staff/test/StaffTest.java
+++ src/staff/test/StaffTest.java
@@ -15,15 +15,22 @@ public class StaffTest {
         printStaffDetails(newStaff);
         printStaffDetails(newLecturer);
 
-        System.out.println(newStaff.equals(newLecturer));
-        System.out.println(newLecturer.equals(newStaff));
+        System.out.println(newStaff.equals(newLecturer)); // false
+        System.out.println(newLecturer.equals(newStaff)); // false
 
         StaffMember newStaff2 = new StaffMember("Junji Ito", 10000, "11/12/2001", "09/06/2021");
         Lecturer newLecturer2 = new Lecturer("Pennywise", 2000, "2/7/2003", "08/06/2021", "CSE", "A");
         Lecturer newLecturerSame = new Lecturer("Junji Ito", 10000, "11/12/2001", "09/06/2021");
 
-        System.out.println(newStaff.equals(newStaff2));
-        System.out.println(newLecturer.equals(newLecturer2));
-        System.out.println(newStaff.equals(newLecturerSame));
+        // reflexive
+        System.out.println(newStaff.equals(newStaff)); // true
+        // symmetric
+        System.out.println(newStaff.equals(newStaff2)); // true
+        System.out.println(newStaff2.equals(newStaff)); // true
+        // null
+        System.out.println(newStaff.equals(null)); // false
+        System.out.println(newLecturer.equals(newLecturer2)); // true
+        System.out.println(newStaff.equals(newLecturerSame)); // true
+        System.out.println(newLecturerSame.equals(newStaff)); // false (overridden equals)
     }
 }
